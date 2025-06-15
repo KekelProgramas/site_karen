3f2d9fe06277c7094b35cbbd5698d6bf0dae51b9
Kekel <71189335+KekelProgramas@users.noreply.github.com>
2025-06-15 04:25:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+function abrirAba(nomeArquivo) {
+    fetch(nomeArquivo)
+    .then(res => res.text())
+    .then(html => {
+        document.querySelector('.conteudo').innerHTML = html;
+    })
+    .catch(err => console.log("Erro ao carregar a aba:", err));
+}
+
+window.onload = function() {
+    abrirAba('Abas/casa.html');
+};
+
+function abrirCarta(carta) {
+  const conteudo = carta.querySelector(".carta-conteudo");
+  if (conteudo.style.display === "block") {
+    conteudo.style.display = "none";
+  } else {
+    conteudo.style.display = "block";
+  }
+}
+
+window.addEventListener('load', () => {
+  document.body.style.visibility = 'visible';
+});
